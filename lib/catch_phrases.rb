e22113caaeacb09ef3ba85b1fb57d9dfea8505fc
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 07:36:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,7 @@
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario
   phrase = "It's-a me, Mario!"
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
